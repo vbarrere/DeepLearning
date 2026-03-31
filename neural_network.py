42a8dca6460947aff73ffbf3fc6ccabd1150dcff
victor.barrere <victor.barrere@cnrs-orleans.fr>
2026-03-31 13:01:33 +0200

    Implement neural network architecture and data preprocessing for training

--- neural_network.py
+++ neural_network.py
@@ -8,5 +8,74 @@ import pandas as pd
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
+from sklearn.model_selection import train_test_split
+
 img_path = "../Data/Data_HRTEM/HRTEM_image"
 data = pd.read_csv("../Data/Data_processed/data.dat", sep="\t", na_values=["nan"])
+
+data["image_file"] = data["i_sim"] + '.png'
+data["eta_parameter"] = 2 * np.abs(data["nat1_out"] / (data["nat1_out"] + data["nat2_out"]) - data["nat1"] / data["n_atoms"]) + 2 * np.abs(data["nat1_in"] / (data["nat1_in"] + data["nat2_in"]) - data["nat1"] / data["n_atoms"]) - data["d_com"] / (2*data["gyration_radius"])
+mask = np.isnan(data["eta_parameter"])
+data = data[~mask]
+
+train_data, test_data = train_test_split(data, test_size=0.2, random_state=42)
+train_data, val_data = train_test_split(train_data, test_size=0.2, random_state=42)
+
+
+datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+train_gen = datagen.flow_from_dataframe(
+    dataframe=train_data,
+    directory=img_path,
+    x_col="image_file",
+    y_col="eta_parameter",
+    class_mode="raw",
+    target_size=(64, 64),
+    batch_size=32,
+    color_mode="grayscale"
+)
+
+val_gen = datagen.flow_from_dataframe(
+    dataframe=val_data,
+    directory=img_path,
+    x_col="image_file",
+    y_col="eta_parameter",
+    class_mode="raw",
+    target_size=(64, 64),
+    batch_size=32,
+    color_mode="grayscale"
+)
+
+test_gen = datagen.flow_from_dataframe(
+    dataframe=test_data,
+    directory=img_path,
+    x_col="image_file",
+    y_col="eta_parameter",
+    class_mode="raw",
+    target_size=(64, 64),
+    batch_size=32,
+    color_mode="grayscale"
+)
+
+
+
+""" Architecture du réseau de neurones """
+
+modele = tf.keras.models.Sequential()
+modele.add(tf.keras.layers.Input(shape=(64, 64, 1)))
+modele.add(tf.keras.layers.Conv2D(32, kernel_size=3, activation='relu'))
+modele.add(tf.keras.layers.Conv2D(32, kernel_size=3, activation='relu'))
+modele.add(tf.keras.layers.MaxPooling2D(pool_size=2))
+modele.add(tf.keras.layers.Conv2D(32, kernel_size=3, activation='relu'))
+modele.add(tf.keras.layers.Conv2D(32, kernel_size=3, activation='relu'))
+modele.add(tf.keras.layers.MaxPooling2D(pool_size=2))
+modele.add(tf.keras.layers.Flatten())
+modele.add(tf.keras.layers.Dense(1024, activation='relu'))
+modele.add(tf.keras.layers.Dense(768, activation='relu'))
+modele.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+
+modele.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(modele.summary())
+modele.fit(train_gen, epochs=5)
+score = modele.evaluate(train_gen, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
